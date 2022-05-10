169b7a6ba4ad53acf9bb9ba65a66223b083404ad
Milan <milanmarkovic1124@gmail.com>
2022-05-10 13:14:40 +0200

    17 - deleting comments and $data array

--- src/Controller/MovieController.php
+++ src/Controller/MovieController.php
@@ -13,22 +13,13 @@
     public function moviePage(){
 
       $query = Drupal::entityQuery('node')
-     //  ->condition('nid', 1)
        ->condition('type', 'movie');
 
       $movies = Node::loadMultiple($query->execute());
 
-    //  $nids = $query->execute();
-
-      $data = array(
-        'name'=>'Robot',
-        'email'=>'robo1992@gmail.com'
-      );
-
       return array(
         '#title'=>'Movie reservation',
         '#theme'=>'information_page',
-        '#items'=>$data,
         '#movies'=>$movies
       );
     }
