153079ac379adfd62894d9214daa7ef08f8a2e05
Milan <milanmarkovic1124@gmail.com>
2022-05-13 14:58:38 +0200

    18 - adding files

---
+++ src/Controller/MovieController.php
@@ -0,0 +1,37 @@
+<?php
+
+  namespace Drupal\movie_reservation\Controller;
+
+  use Drupal;
+  use Drupal\Core\Controller\ControllerBase;
+  use Drupal\node\Entity\Node;
+  use Symfony\Component\HttpFoundation;
+
+
+  class MovieController {
+
+    public function moviePage(){
+
+      $genres = Drupal::entityTypeManager()
+        ->getStorage('taxonomy_term')
+        ->loadByProperties(['vid' => 'movie_type']);
+
+      $id = Drupal::request()->query->get('movie_type') ;
+
+      $query = Drupal::entityQuery('node')
+       ->condition('type', 'movie');
+
+      if(!empty($id)) {
+        $query->condition('field_movie_type', $id);
+      }
+
+      $movies = Node::loadMultiple($query->execute());
+
+      return array(
+        '#title'=>'Movie reservation',
+        '#theme'=>'information_page',
+        '#movies'=>$movies,
+        '#genres'=>$genres
+      );
+    }
+  }
