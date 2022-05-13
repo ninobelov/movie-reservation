9ca60f990537f88ea1ca080f2a2603e60935d12d
Milan Markovic <micomarkovic1997@gmail.com>
2022-05-13 16:24:59 +0200

    18 - Add select (dropdown list) on movie reservation page (#4)
    * 18 - adding files
    
    * 18 - deleting unnecessary file
    
    Co-authored-by: Milan <milanmarkovic1124@gmail.com>

---
+++ js/movie_reservation.js
@@ -0,0 +1,10 @@
+  $("#genre").on("change", function () {
+    $.ajax({
+      url: "./movie_reservation?movie_type=" +$("#genre").val(),
+      type: "GET",
+      success: function () {
+        window.location.href = "./movie_reservation?movie_type=" + $("#genre").val();
+      }
+    });
+  })
+
