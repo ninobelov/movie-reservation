89c3ca6eeff499b4bffbdb5aa3c1eb874f72209a
Milan <milanmarkovic1124@gmail.com>
2022-05-17 09:19:29 +0200

    22 - adding files

---
+++ js/movie_reservation.js
@@ -0,0 +1,14 @@
+function movietype(){
+$("#genre").on("change", function () {
+    $.ajax({
+      url: "./movie_reservation?movie_type=" +$("#genre").val(),
+      type: "GET",
+      success: function () {
+        window.location.href = "./movie_reservation?movie_type=" + $("#genre").val();
+      }
+    });
+  })
+  }
+
+  movietype();
+
