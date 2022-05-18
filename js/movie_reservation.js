4a5ee030749876182dc4ed03eef2fed38133e137
Milan <milanmarkovic1124@gmail.com>
2022-05-18 13:46:59 +0200

    27 - adding files

---
+++ js/movie_reservation.js
@@ -0,0 +1,25 @@
+function movieGenreChange(){
+$("#movie_genre").on("change", function () {
+    $.ajax({
+      url: "./movie_reservation?movie_type=" +$("#movie_genre").val(),
+      type: "GET",
+      success: function () {
+        window.location.href = "./movie_reservation?movie_type=" + $("#movie_genre").val();
+      }
+    });
+  })
+  }
+
+function nameValidationReq() {
+  $(".btn-submit").click(function(){
+    let name = $('#customer_name').val();
+    let nameVal = /^[A-Z][a-z]+$/;
+    if (nameVal.test(name)) {
+      alert("Good, thank you!");
+    } else {
+      alert("Error! Please use first capital letter and don't use numbers in the name section, thank you!");
+    }
+  })
+}
+  nameValidationReq();
+  movieGenreChange();
